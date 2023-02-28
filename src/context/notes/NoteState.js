70fa45d5f5f44135bd4eef318f5f0cb904e8e429
ghazanfarali1994 <ghazanfar006@gmail.com>
2023-02-28 23:07:08 +0500

    adding Noteitem in separate component 61 completed

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,21 +2,76 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) =>{
-    const s1 = {
-        "name": "Rana",
-        "class": "5b"
-    }    
-    const [state, setState] = useState(s1)
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name": "Rajpoot",
-                "class": "10b"
-            }   )
-        }, 1000);
-    }
+    const notesInitial = [
+        {
+          "_id": "63fb955777819daa1d0dd28b",
+          "user": "63f7b50f1a775142311a3983",
+          "title": "New Note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2023-02-26T17:22:31.567Z",
+          "__v": 0
+        },
+        {
+          "_id": "63fe38d095ae963c88e4fe66",
+          "user": "63f7b50f1a775142311a3983",
+          "title": "New Note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2023-02-28T17:24:32.490Z",
+          "__v": 0
+        },
+        {
+          "_id": "63fe38d395ae963c88e4fe68",
+          "user": "63f7b50f1a775142311a3983",
+          "title": "New Note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2023-02-28T17:24:35.093Z",
+          "__v": 0
+        },
+        {
+          "_id": "63fe38d495ae963c88e4fe6a",
+          "user": "63f7b50f1a775142311a3983",
+          "title": "New Note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2023-02-28T17:24:36.369Z",
+          "__v": 0
+        },
+        {
+          "_id": "63fe38d595ae963c88e4fe6c",
+          "user": "63f7b50f1a775142311a3983",
+          "title": "New Note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2023-02-28T17:24:37.318Z",
+          "__v": 0
+        },
+        {
+          "_id": "63fe38d595ae963c88e4fe6e",
+          "user": "63f7b50f1a775142311a3983",
+          "title": "New Note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2023-02-28T17:24:37.651Z",
+          "__v": 0
+        },
+        {
+          "_id": "63fe38d595ae963c88e4fe70",
+          "user": "63f7b50f1a775142311a3983",
+          "title": "New Note",
+          "description": "Please access the playlist",
+          "tag": "Youtube",
+          "date": "2023-02-28T17:24:37.888Z",
+          "__v": 0
+        }
+      ]
+
+      const [notes, setNotes] = useState(notesInitial)
+  
     return (
-        <NoteContext.Provider value={{state, update}} >
+        <NoteContext.Provider value={{notes, setNotes}} >
             {props.children}
         </NoteContext.Provider>
     )
