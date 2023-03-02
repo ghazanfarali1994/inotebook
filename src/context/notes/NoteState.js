3d62b24f296aced6c9479bbc854d589d9f362707
ghazanfarali1994 <ghazanfar006@gmail.com>
2023-03-02 22:59:20 +0500

    adding addnote component 63 completed

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -69,9 +69,36 @@ const NoteState = (props) =>{
       ]
 
       const [notes, setNotes] = useState(notesInitial)
+
+      // Add a Note
+      const addNote = (title, description, tag) => {
+        // TODO: API Call
+        console.log("Adding a new note")
+        const note = {
+          "_id": "63fe38d595ae963c88e4fe70",
+          "user": "63f7b50f1a775142311a3983",
+          "title": title,
+          "description": description,
+          "tag": tag,
+          "date": "2023-02-28T17:24:37.888Z",
+          "__v": 0
+        };
+        setNotes(notes.concat(note))
+      }
+
+      // Delete a Note
+      const deleteNote = () => {
+        
+      }
+
+      // Edit a Note 
+      const editNote = () => {
+        
+      }
+
   
     return (
-        <NoteContext.Provider value={{notes, setNotes}} >
+        <NoteContext.Provider value={{notes, addNote, deleteNote, editNote}} >
             {props.children}
         </NoteContext.Provider>
     )
