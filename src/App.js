e94b33e6b0e5d8b4c501cd3b94e727751b9d1c1a
ghazanfarali1994 <ghazanfar006@gmail.com>
2023-03-15 18:12:10 +0500

    adding signup component 70

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
-import { Home } from './components/Home';
+import { Home } from './components/Home'; 
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
 import { Alert } from './components/Alert';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="about" element={<About />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
           </Routes>
         </div>
       </NoteState>
